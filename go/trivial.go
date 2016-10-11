27f2be4e75b3b83bd4894e6262d2a41653771692
Ben Sigelman <bhs@lightstep.com>
2016-10-10 22:20:26 -0700

    Commit a trivial example to reference on OT.io

---
+++ go/trivial.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/opentracing/opentracing-go"
+	zipkin "github.com/openzipkin/zipkin-go-opentracing"
+)
+
+func main() {
+	// 1) Create a opentracing.Tracer that sends data to Zipkin
+	collector, _ := zipkin.NewHTTPCollector(
+		fmt.Sprintf("http://%s:9411/api/v1/spans", os.Args[1]))
+	tracer, _ := zipkin.NewTracer(
+		zipkin.NewRecorder(collector, false, "127.0.0.1:0", "trivial"))
+
+	// 2) Demonstrate simple OpenTracing instrumentation
+	parent := tracer.StartSpan("Parent")
+	for i := 0; i < 20; i++ {
+		parent.LogEvent(fmt.Sprintf("Starting child #%d", i))
+		child := tracer.StartSpan("Child", opentracing.ChildOf(parent.Context()))
+		time.Sleep(50 * time.Millisecond)
+		child.Finish()
+	}
+	parent.LogEvent("A Log")
+	parent.Finish()
+
+	// ... give Zipkin ample time to flush
+	time.Sleep(2 * time.Second)
+}
